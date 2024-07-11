64bfbe448b6ba034c4a8a3fee3c7a3a473d23f99
unknown <virgiawan753@gmail.com>
2024-07-11 13:49:49 +0700

    skripsi

---
+++ app.py
@@ -0,0 +1,31 @@
+import joblib
+import streamlit as st
+
+# Load the saved model and scaler
+model = joblib.load('stunting_svm.pkl')
+scaler = joblib.load('scaler.pkl')
+
+# Define the application layout
+st.title('Stunting Prediksi App')
+
+# Get user input
+gender = st.radio('Gender', ['Male', 'Female'])
+height = st.number_input('Tinggi Badan (cm)', min_value=0.0)
+weight = st.number_input('Berat Badan (kg)', min_value=0.0)
+
+# Preprocess the user input
+input_data = [[gender, height, weight]]
+input_scaled = scaler.transform(input_data)
+
+# Make a prediction
+if st.button('Predict'):
+    prediction = model.predict(input_scaled)[0]
+    index_labels = {
+       1: 'Tinggi',
+       2: 'Normal',
+       3: 'Stunting',
+       4: 'Severly Stunting',
+       5: 'Extremely Stunting'
+    }
+    st.write(f'Predicted Stunting Category: {index_labels[prediction]}')
+
